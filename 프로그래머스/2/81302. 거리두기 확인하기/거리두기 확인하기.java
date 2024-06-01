166d752de625f2513778c5b5d4764669ad6f2b9f
Wooseok <wooseokbird@gmail.com>
2024-06-01 16:26:49 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.57 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -6,74 +6,92 @@ class Solution {
     public static int[] ans = {1,1,1,1,1};
     
     public int[] solution(String[][] places) {
+        int[] answer = {};
         int rowLen = places.length;
         int colLen = places[0].length;
         
-        Character[][] grid = new Character[5][5];
-        
-        for(int i =0; i < 5; i++){
+        for(int i = 0; i < 5; i++) {
             String[] rooms = places[i];
             
-            
-            for(int r =0; r < rowLen; r++) {
+            Character[][] grid = new Character[rowLen][colLen];
+            // String[][] -> Character[][] 
+            for(int r = 0; r < rowLen; r++) {
                 for(int c = 0; c < colLen; c++) {
                     grid[r][c] = rooms[r].charAt(c);
                 }
             }
-            
-            for(int r =0; r < rowLen; r++) {
-                for(int c = 0; c < colLen; c++) {
+
+            for(int r = 0; r < rowLen; r++) {
+                for(int c = 0 ; c < colLen; c++) {
                     if(grid[r][c] == 'P') {
-                        bfs(r,c,grid,i);
+                        bfs(grid, r,c, i);
                     }
                 }
-            }     
+            }        
+            
+            
         }
         
-        
         return ans;
+        
     }
-    
-    public static void bfs(int r, int c, Character[][] grid, int idx) {
+    public static void bfs(Character[][] grid, int r , int c, int idx) {
+        int rowLen = grid.length;
+        int colLen = grid[0].length;
+
+        Deque<Point> q = new ArrayDeque<>();
+        q.offer(new Point(r,c, 1));
         
-        
-        Deque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{r,c});
-        boolean[][] visited = new boolean[5][5];
+        boolean[][] visited = new boolean[rowLen][colLen];
         visited[r][c] = true;
         
         while(!q.isEmpty()) {
-            int[] cur = q.poll();
-            int curRow = cur[0];
-            int curCol = cur[1];
-            
-            for(int i =0; i < 4; i++) {
+            Point cur = q.poll();
+            int curRow = cur.r;
+            int curCol = cur.c;
+            int curStep = cur.step;
+            for(int i = 0 ; i < 4 ;i++) {
                 int nr = curRow + dr[i];
                 int nc = curCol + dc[i];
                 
-                if( ( 0 <= nr && nr < 5) && (0 <= nc && nc < 5) ) {                    
-                    // if(visited[nr][nc]) break;
-                    if(nr == r && nc == c) continue;
+                if( (0 <= nr && nr < rowLen)  && (0 <= nc && nc < colLen ) ) {
                     
-                    int depth = Math.abs(nr - r) + Math.abs(nc - c);
-
-                    // if(grid[nr][nc] == 'X') break;
-
-                    if(grid[nr][nc] == 'P' && depth <= 2) {
-                        ans[idx] = 0;
-                        return;
-                    } 
-                    if(grid[nr][nc] == 'O' && depth < 2) {
-                        q.offer(new int[]{nr, nc});
+                    if(visited[nr][nc]) continue;
+                    if(grid[nr][nc] == 'X') continue;
+                    
+                    if(grid[nr][nc] == 'O') {
+                        q.offer(new Point(nr,nc,curStep+1));
                         visited[nr][nc] = true;
                     }
-                    
+                    if(grid[nr][nc] == 'P' && curStep <= 2) {
+                        ans[idx] = 0;
+                        return ;
+                    }
                 }
+                
             }
-            
         }
         
     }
-
     
+    public static class Point {
+        int r, c, step;
+        public Point(int r, int c, int step) {
+            this.r = r;
+            this.c = c;
+            this.step = step;
+        }
+    }
+    
+    
+    
+    public static void print(Character[][] grid) {
+        for(Character[] c: grid) {
+            for(Character d : c) {
+                System.out.printf("%c ", d);    
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
