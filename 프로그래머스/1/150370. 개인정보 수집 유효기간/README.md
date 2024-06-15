53e6ec5baef68c4b1bbb457feee469d7ea3cfa59
Wooseok <wooseokbird@gmail.com>
2024-06-15 14:04:50 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.92 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
@@ -1,10 +1,10 @@
 # [level 1] 개인정보 수집 유효기간 - 150370 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370) 
 
 ### 성능 요약
 
-메모리: 78.6 MB, 시간: 14.81 ms
+메모리: 75 MB, 시간: 0.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 15일 13:41:18
+2024년 06월 15일 14:04:48
 
 ### 문제 설명
 
--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,75 +1,38 @@
 import java.util.*;
 class Solution {
-    static List<Integer> arr = new ArrayList<>();
     public int[] solution(String today, String[] terms, String[] privacies) {
-        
-        
+        List<Integer> arr = new ArrayList<>();
         Map<String, Integer> map = new HashMap<>();
-        for(String str: terms)  {
-            String[] temp = str.split(" ");
-            Integer num = Integer.parseInt(temp[1]);
-            map.put(temp[0], num);
+        for(String str: terms) {
+            StringTokenizer st = new StringTokenizer(str, " ");
+            String type = st.nextToken();
+            Integer num = Integer.parseInt(st.nextToken()) * 28;
+            map.put(type, num);
         }
         
-        for(int idx = 0; idx < privacies.length; idx++) {
-
-            String[] temp = privacies[idx].split(" ");
-            String date = temp[0];
-            String target = temp[1];
-            if(map.containsKey(target)) {
-                Integer plusNum = map.get(target);
-                StringTokenizer stk =new StringTokenizer(date, ".");
-	            
-                Integer year = Integer.parseInt(stk.nextToken());
-                Integer month = Integer.parseInt(stk.nextToken());
-                Integer day = Integer.parseInt(stk.nextToken());
-
-                
-
-                // Integer newYear = year + ((month + plusNum) / 12);
-                Integer newYear = year;
-                if((month + plusNum) > 12) {
-                    newYear += (month + plusNum) / 12;
-                    if((month + plusNum) % 12 == 0 ) {
-                        newYear-=1;  
-                    } 
-                }
-                
-                
-                Integer newMonth = (month + plusNum) % 12 ; 
-                if(newMonth == 0) {
-                    newMonth = 12;
-                }
-                
-                
-                
-                StringTokenizer stk2 =new StringTokenizer(today, ".");
-                Integer todayYear = Integer.parseInt(stk2.nextToken());
-                Integer todayMonth = Integer.parseInt(stk2.nextToken());
-                Integer todayDay = Integer.parseInt(stk2.nextToken());
-                System.out.printf("%d %d %d\n", newYear, newMonth, day);
-                // 오늘이 2020, 새로운게 2019이면 지나서 추가.
-                if(todayYear > newYear) {
-                    arr.add(idx+1);
-                } else if(todayYear < newYear) { // 오늘이 2020, 새로운게  2021이면 아직남았어
-                    continue;
-                }
-                else if(newMonth < todayMonth ){ // year는같아, month비교.
-                    arr.add(idx+1);
-                } else if(newMonth <= todayMonth && todayDay >= day) {
-                    arr.add(idx+1);
-                }
-                
+        int todayDays = daysFrom2000(today);
+        for(int i = 0; i < privacies.length; i++) {
+            StringTokenizer st = new StringTokenizer(privacies[i], " ");
+            int signDays = daysFrom2000(st.nextToken());
+            int elapsedDay = todayDays - signDays;
+            if(elapsedDay >= map.get(st.nextToken())) {
+                arr.add(i+1);
             }
         }
-            
-        
-          
         
         int[] ans = new int[arr.size()];
         for(int i =0; i < arr.size(); i++)
             ans[i] = arr.get(i);
         return ans;
-        
+    }
+    
+    
+    public static int daysFrom2000(String date) {
+        StringTokenizer st = new StringTokenizer(date, ".");
+        int result = 0;
+        result += (Integer.parseInt(st.nextToken()) - 2000) * 336;
+        result += (Integer.parseInt(st.nextToken()) -1 ) * 28;
+        result += Integer.parseInt(st.nextToken()) - 1;
+        return result;
     }
 }
