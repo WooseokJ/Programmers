7aa029272d712e1029695dcfd78045e881b19f77
Wooseok <wooseokbird@gmail.com>
2024-07-11 20:22:40 +0900

    [level 2] Title: 피로도, Time: 0.75 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,59 +2,53 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int[][] dungeons) {
-        int ans = 0;
-        int init_k = k;
-        // arr 에는 열의 개수만큼의 index에 대한 순열 값들이 있다. 
-        List<List<Integer>> arr = permutation(dungeons);
-
-        for(List<Integer> indexArr : arr) {
-            int cnt = 0;
-            k = init_k;  // k 초기화.
-            // 새로운 인덱스 배열로 시도
-            for(Integer i: indexArr) {
-                int minRequiredPirodo = dungeons[i][0];
-                int consumePirodo = dungeons[i][1];
-                if(k >= minRequiredPirodo) { // 최소 필수 피로도 이상이면.
-                    k-= consumePirodo;
-                    cnt++;
-                }     
-            }
-            // 배열 시도 이전의 값중 가장큰값(ans) vs 새로운 인덱스 배열시도(cnt)
-            ans = Math.max(ans, cnt);
-        }
         
-
-        return ans;
-    
-    }
-    // 순열 
-    public List<List<Integer>> permutation(int[][] dungeons) {
-        // dungeons의 최소필요피로도 개수만큼의 index값 갖는 nums 생성.
-        int n = dungeons.length;
-        int[] nums = new int[n]; 
-        for(int i = 0; i < n; i++) {
-            nums[i] = i;
-        }
-        // 순열 템플릿
+        
+        // 최소피로도, 소모피로도 
+        
         List<List<Integer>> ans = new ArrayList<>();
-        backtrack(new ArrayList<>(), nums, ans);
-        return ans;
-    }
-    public static void backtrack(List<Integer> curr, int[] nums, List<List<Integer>> ans) {
-        if(curr.size() == nums.length) {
-            ans.add(new ArrayList<>(curr));
+        int n = dungeons.length;
+        boolean[] visited = new boolean[n];
+        backtrack(new ArrayList<>(), n, visited, ans);
+        
+        int max = 0;
+        int init_k = k;
+        for(List<Integer> idxList: ans) {
+            int val = 0;
+            k = init_k;
+            for(Integer i: idxList) {
+                int minPirodo = dungeons[i][0];
+                int consumePirodo = dungeons[i][1];
+                if(k >= minPirodo) {
+                    k-= consumePirodo;
+                    val++;
+                } else {
+                    break;
+                }
+            }
             
-            return ;
+            max = Math.max(max, val);
         }
         
-        for(int num: nums) {
-            if(!curr.contains(num)) {
-                curr.add(num);
-                backtrack(curr, nums, ans);
+        return max;
+        
+    }
+    
+    
+    public static void backtrack(List<Integer> curr, int n, boolean[] visited, List<List<Integer>> ans) {
+        if(curr.size() == n) {
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        
+        for(int i = 0 ; i < n; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                curr.add(i);
+                backtrack(curr, n, visited, ans);
                 curr.remove(curr.size()-1);
+                visited[i] = false;
             }
         }
     }
-    
-    
 }
