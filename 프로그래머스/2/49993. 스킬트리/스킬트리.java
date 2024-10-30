0698abfc70c841fd86ed9f52ba7b924469df4493
Wooseok <wooseokbird@gmail.com>
2024-10-30 13:17:23 +0900

    [level 2] Title: 스킬트리, Time: 0.77 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,33 +1,25 @@
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        int ans =0;
+        int ans = 0;
+        
         for(String str: skill_trees) {
-            int cnt = 0;
-            
-            for(int i =0; i< str.length(); i++) {
-                char curWord = str.charAt(i);
-                // 현재 기술이 skill에 포함되는지
-                if(skill.indexOf(curWord) != -1) {
-                    // 순서대로 배웟는지 확인
-                    char stand = skill.charAt(cnt);
-                    if(stand == curWord) {
-                        cnt++;
-                    } else {
-                        //순서대로 안배우면 안쪽 for문 빠져나와
-                        break;
-                    }
-                }
+            StringBuilder sb = new StringBuilder();
+            for(int i =0; i < str.length(); i++) {
+                Character c = str.charAt(i);
+                String cc = String.valueOf(c);
                 
-                // 끝까지 다 간건 스킬대로 배운거니 증가
-                if(i== str.length()-1) {
-                    ans++;
-                }
-                
+                if(skill.contains(cc))
+                    sb.append(c); // 순서있게 붙여.
             }
             
+            // System.out.println(sb.toString() +" " + skill.indexOf(sb.toString()));
             
+            if(skill.indexOf(sb.toString()) == 0) {
+                ans ++;
+            }
+
         }
-        return ans;
         
+        return ans;
     }
 }
