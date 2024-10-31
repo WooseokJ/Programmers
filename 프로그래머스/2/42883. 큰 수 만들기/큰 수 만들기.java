05069003a975b4988a4b33b6ee7e32a8222c70fb
Wooseok <wooseokbird@gmail.com>
2024-10-31 14:36:19 +0900

    [level 2] Title: 큰 수 만들기, Time: ., Memory: undefined -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,38 +1,10 @@
 import java.util.*;
 class Solution {
     public String solution(String number, int k) {
-        int n = number.length();
-        int num = n-k;
+        String ans = "";
+        List<Integer> arr = new ArrayList<>();
+        arr.sort(Integer:: compareTo);
         
-        Deque<Character> stack = new ArrayDeque<>();
-        for(int i = 0; i < n; i++) {
-            char bottom = number.charAt(i);
-            
-            while(!stack.isEmpty()) {
-                char top = stack.peekLast();
-                if(top < bottom) {
-                    // k == 0이면 -1이 되서 charAt(-1) = null되므로. 
-                    if(k <= 0) break;
-                    stack.pollLast(); // pop
-                    k--;
-                    
-                } else {
-                    break;
-                }
-                
-            }
-            
-            
-            stack.offerLast(bottom);
-            
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < num; i++)
-            sb.append(stack.pollFirst());
-        
-        
-        
-        return sb.toString();
+        return ans;
     }
-}   
+}
