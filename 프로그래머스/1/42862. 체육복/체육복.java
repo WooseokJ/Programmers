40deaffc6e0d8dbb231541afb7f04c434a5170f6
Wooseok <wooseokbird@gmail.com>
2024-10-31 12:15:13 +0900

    [level 1] Title: 체육복, Time: 6.88 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,58 +1,41 @@
 import java.util.*;
 class Solution {
-    public int solution(int n, int[] l, int[] reserve) {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int ans = 0;
+        ans = n - lost.length;
         
-        List<Integer> lost = new ArrayList<>();
-        for(int temp: l)
-            lost.add(temp);
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
         
-        List<Integer> students = new ArrayList<>();
-        for(int i =1; i<= n; i++)
-            if(!lost.contains(i)) {
-                students.add(i);
-            }
-       
-       
-       
-        // student 7 8 9 10 
-        // lost, 1 2 3 4 5 6
-        // reserve 1 2 3 , 
-        // 
-
-        for(int add: reserve) {
-            if(lost.contains(add)) {
-                if(!students.contains(add)) {
-                    students.add(add);    
+        for(int i =0; i < lost.length; i++) {
+            for(int j = 0; j < reserve.length; j++) {
+                // 여벌갖은 학생이 도난경우.
+                if(lost[i] == reserve[j]) {
+                    ans++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
                 }
-                continue;   
             }
-            
-            int pre = add-1;
-            int back = add +1;
-            
-            // 2, 4 
-            if(!students.contains(pre)) {
-                if(pre != 0) {
-                    students.add(pre);   
+        }
+        
+        for(int i = 0; i < lost.length; i++) {
+            for(int j = 0; j < reserve.length; j++) {
+                if(lost[i] - 1 == reserve[j] || 
+                  lost[i]  +1 == reserve[j])  {
+                    ans++;
+                    reserve[j] = -1;
+                    break;
                 }
-                
-            } else if(!students.contains(back)) {
-                if(back != n+1) {
-                    students.add(back);   
-                }   
             }
         }
         
         
-        return students.size();
-        
-
+        return ans;
     }
 }
-
-
-
-// 1 -> 2,0
-// 3 -> 4,2
-// 5-> 4,6
-
+//4 -> 3 5 
+// n : 전체 학생
+// lost: 도난 학생번호
+// reserve: 여벌 체육복 갖은 학생. -> 2,4둘중하
+// return: 체육듣는 최대학생. 
