b3f376f84918af69f5e42dc06efd2a3ad5f1c202
Wooseok <wooseokbird@gmail.com>
2024-10-31 11:58:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.73 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,64 +1,57 @@
 import java.util.*;
 
 class Solution {
+    static int rowLen;
+    static int colLen;
     public int solution(int[][] maps) {
         int ans = 0;
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
-        ans = bfs(visited,maps);
+        rowLen = maps.length;
+        colLen = maps[0].length;
+        
+        boolean[][] visited = new boolean[rowLen][colLen];
+        ans = bfs(visited, maps);
+        
         
         return ans;
     }
-    public static int bfs(boolean[][] visited, int [][] maps) {
-        int rowLen = maps.length;
-        int colLen = maps[0].length;
-        visited[0][0] = true;
+    public static int bfs(boolean[][] visited, int[][] maps) {
         int[] dr = {1,-1,0,0};
         int[] dc = {0,0,1,-1};
         
-        Deque<Pair> q = new ArrayDeque<>();
-        q.offer(new Pair(0,0,1));
+        visited[0][0] = true;
+        Deque<Point> q = new ArrayDeque<>();
+        q.offer(new Point(0,0,1));
+        
         while(!q.isEmpty()) {
-            Pair cur = q.poll();
-            int curRow = cur.r;
-            int curCol = cur.c;
-            int curStep = cur.step;
-            for(int i =0; i < 4; i++) {
-                int nextRow = curRow + dr[i];
-                int nextCol = curCol + dc[i];
+            Point cur = q.poll();
+            for(int i =0 ;i < 4; i++) {
+                int nr = cur.r + dr[i];
+                int nc = cur.c + dc[i];
                 
-                if(isValid(nextRow, nextCol, rowLen, colLen)) {
-                    if(maps[nextRow][nextCol] == 1) {
-                        if(!visited[nextRow][nextCol]) {
-                            if(nextRow == rowLen-1 && nextCol == colLen-1) {
-                                
-                                return curStep+1;
-                            }
-                            visited[nextRow][nextCol] = true;
-                            q.offer(new Pair(nextRow, nextCol, curStep+1));
-                            
-                        }                       
+          
+                
+                if((0<=nr && nr < rowLen) && (0 <= nc && nc < colLen)) {
+                    if(maps[nr][nc] == 1 && nr == rowLen-1 && nc == colLen-1) {
+                        return cur.step+1;
                     }
-                 
+                    
+                    if(!visited[nr][nc] && maps[nr][nc] == 1) {
+                        q.offer(new Point(nr,nc,cur.step+1));
+                        visited[nr][nc] = true;
+                            
+                    }
                 }
             }
         }
+        
         return -1;
-        
-        
     }
-    public static boolean isValid(int r, int c, int rowLen, int colLen) {
-        
-        return (( 0<= r && r < rowLen) && (0 <= c && c < colLen));
-    }
-    public static class Pair {
+    public static class Point {
         int r, c, step;
-        public Pair(int r, int c, int step) {
+        Point(int r, int c, int step) {
             this.r = r;
             this.c = c;
             this.step = step;
         }
-        
     }
-    
-    
 }
