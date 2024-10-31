065f6bf68dc4581fe3cec9b0c0a058a10f25e7a5
Wooseok <wooseokbird@gmail.com>
2024-10-31 14:22:01 +0900

    [level 3] Title: 단어 변환, Time: 0.97 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,6 +1,54 @@
+import java.util.*;
+
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        return answer;
+        int ans = 0;
+        
+        int n = words.length;
+        boolean[] visited = new boolean[n];
+        Deque<Info> q = new ArrayDeque<>();
+        q.offer(new Info(0, begin));
+        
+        while(!q.isEmpty()) {
+            Info cur = q.poll();
+            if(cur.word.equals(target)) return cur.step;
+            
+            for(int i = 0; i < words.length; i++) {
+                String word = words[i];
+                // 현재 단어 아니면 diff계산.
+                if(!word.equals(cur.word)) {
+                    int diff = 0;
+                    for(int j =0 ; j < word.length(); j++) {
+                        int front = word.charAt(j);
+                        int back = cur.word.charAt(j);
+                        if(front != back)
+                            diff++;
+                    }
+                    
+                    if(diff == 1 && !visited[i]) {
+                        visited[i] = true;
+                        q.offer(new Info(cur.step+1, word));
+                    }
+                    
+                }
+                
+            }
+        }
+            
+        
+            
+  
+        
+        
+        return 0;
+    }
+    
+    public static class Info {
+        int step;
+        String word;
+        Info(int step, String word) {
+            this.step = step;
+            this.word = word;
+        }
     }
 }
