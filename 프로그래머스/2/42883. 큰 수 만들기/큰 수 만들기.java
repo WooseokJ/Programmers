898c38538c042db82ea4118ce778c7271464238d
Wooseok <wooseokbird@gmail.com>
2024-10-31 14:35:43 +0900

    [level 2] Title: 큰 수 만들기, Time: 71.27 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,37 +1,38 @@
 import java.util.*;
 class Solution {
     public String solution(String number, int k) {
-        char[] result = new char[number.length() - k];
-        int n = number.length() - k;
+        int n = number.length();
+        int num = n-k;
+        
         Deque<Character> stack = new ArrayDeque<>();
-        for(int i =0; i< number.length(); i++) {
-            char c = number.charAt(i);
-            while(!stack.isEmpty() && stack.peekLast() < c && k-- > 0) {
-                stack.pollLast();        
+        for(int i = 0; i < n; i++) {
+            char bottom = number.charAt(i);
+            
+            while(!stack.isEmpty()) {
+                char top = stack.peekLast();
+                if(top < bottom) {
+                    // k == 0이면 -1이 되서 charAt(-1) = null되므로. 
+                    if(k <= 0) break;
+                    stack.pollLast(); // pop
+                    k--;
+                    
+                } else {
+                    break;
+                }
+                
             }
-            stack.offerLast(c);
+            
+            
+            stack.offerLast(bottom);
+            
         }
-        
-        
-        
         
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < num; i++)
             sb.append(stack.pollFirst());
-        }
         
         
-        
-  
         
         return sb.toString();
     }
-}
-
-
-
-
-// k: 4
-// 정답: 987654 ✅
-// 실제: 98765 ❌
-// '1', '10'
+}   
