cb426d9374b479c36bd097a08835efe54ce00f6e
Wooseok <wooseokbird@gmail.com>
2024-12-15 11:07:59 +0900

    [level 2] Title: 스킬트리, Time: 0.23 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/README.md
+++ 프로그래머스/2/49993. 스킬트리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 스킬트리 - 49993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993) 
 
 ### 성능 요약
 
-메모리: 80.9 MB, 시간: 0.28 ms
+메모리: 82.8 MB, 시간: 0.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 31일 12:04:45
+2024년 12월 15일 11:07:56
 
 ### 문제 설명
 
--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int ans = 0;
@@ -5,17 +6,14 @@ class Solution {
         for(String str: skill_trees) {
             StringBuilder sb = new StringBuilder();
             
-            for(int i = 0 ; i < str.length(); i++) {
-                char c = str.charAt(i);
-                String temp = String.valueOf(c);
-                if(skill.contains(temp)) {
+            for(int i= 0; i < str.length(); i++) {
+                String c = String.valueOf(str.charAt(i));
+                if(skill.contains(c))
                     sb.append(c);
-                }
             }
-            String s = sb.toString();
-            if(skill.indexOf(s) == 0)
-                ans++;
             
+            if(skill.indexOf(sb.toString()) == 0)
+                ans++;
         }
         
         
