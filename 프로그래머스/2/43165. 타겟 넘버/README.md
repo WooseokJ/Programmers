39417d691b67bcb3c1da89fd7726077aee38750e
Wooseok <wooseokbird@gmail.com>
2024-12-16 16:16:38 +0900

    [level 2] Title: 타겟 넘버, Time: 7.60 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 307 MB, 시간: 408.37 ms
+메모리: 85.4 MB, 시간: 7.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 31일 13:31:00
+2024년 12월 16일 16:16:35
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,47 +1,24 @@
 import java.util.*;
-class Solution {
-    public int solution(int[] numbers, int target) {
-        int ans = 0;
-        List<List<Integer>> arr = new ArrayList<>();
-        int n = numbers.length;
-        for(int i =0; i < n; i++)
-            backtrack(new ArrayList<>(), 0, n, i, arr);
-        
 
-        
-        for(List<Integer> a: arr) {
-            int[] copy = Arrays.copyOf(numbers, n);
-            
-            for(Integer b: a) {
-                copy[b] *= -1;
-            }
-            int sum = 0;
-            for(int t: copy)
-                sum+=t;
-            if(sum == target) ans++;
-        }
-            
-        
-        
-        
+class Solution {
+    static int ans = 0;
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
         return ans;
     }
-    public static void backtrack(List<Integer> cur, int start, int n ,int k, List<List<Integer>> ans) {
-        if(cur.size() == k) {
-            ans.add(new ArrayList<>(cur));
-            return;
-        }
-        for(int i = start; i < n; i++) {
-            cur.add(i);
-            backtrack(cur, i+1, n,k,ans);
-            cur.remove(cur.size()-1);
-                
+    public static void dfs(int[] numbers, int target, int step, int sum) {
+        if(step == numbers.length) {
+            if(target == sum)
+                ans++;
+            return;    
         }
         
-        
+        int plus = sum + numbers[step];
+        int minus = sum - numbers[step];
+        dfs(numbers, target, step+1, plus);
+        dfs(numbers, target, step+1, minus);
             
         
-            
         
     }
 }
