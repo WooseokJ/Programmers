fc26726a9d51d68e05a8c20ad5ec0864f19617ce
Wooseok <wooseokbird@gmail.com>
2024-12-16 17:07:24 +0900

    [level 2] Title: 타겟 넘버, Time: 86.28 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
 
 ### 성능 요약
 
-메모리: 85.4 MB, 시간: 7.60 ms
+메모리: 118 MB, 시간: 86.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 16일 16:16:35
+2024년 12월 16일 17:07:22
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,24 +1,30 @@
 import java.util.*;
-
 class Solution {
     static int ans = 0;
+    
     public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0, 0);
+        bfs(numbers, target,0);
         return ans;
     }
-    public static void dfs(int[] numbers, int target, int step, int sum) {
-        if(step == numbers.length) {
-            if(target == sum)
-                ans++;
-            return;    
+    public static void bfs(int[] numbers, int target, int step) {
+        Deque<Integer> q = new ArrayDeque<>();
+        q.offer(0);
+
+        // q에 합에대한게 담겨.
+        for(int num: numbers) {
+            int size = q.size();
+            for(int i= 0; i< size; i++) {
+                int sum = q.poll();
+                q.offer(sum + num); // 0 + 4 -> 4 + 1
+                q.offer(sum - num); // 0 - 4 -> 4 - 1 
+            }
         }
-        
-        int plus = sum + numbers[step];
-        int minus = sum - numbers[step];
-        dfs(numbers, target, step+1, plus);
-        dfs(numbers, target, step+1, minus);
-            
-        
-        
+
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            if(cur == target) {
+                ans++;
+            }
+        }
     }
 }
