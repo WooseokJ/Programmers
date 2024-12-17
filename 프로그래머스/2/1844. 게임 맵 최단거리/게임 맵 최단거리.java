773b3edafe0aecc817e6929b910ef372d6f3b9a3
Wooseok <wooseokbird@gmail.com>
2024-12-17 15:41:42 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.25 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,57 +1,47 @@
 import java.util.*;
-
 class Solution {
-    static int rowLen;
-    static int colLen;
+    static int ans = 0;
     public int solution(int[][] maps) {
-        int ans = 0;
-        rowLen = maps.length;
-        colLen = maps[0].length;
         
-        boolean[][] visited = new boolean[rowLen][colLen];
-        ans = bfs(visited, maps);
-        
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        bfs(0,0,maps, visited);
         
         return ans;
     }
-    public static int bfs(boolean[][] visited, int[][] maps) {
-        int[] dr = {1,-1,0,0};
-        int[] dc = {0,0,1,-1};
+    public static void bfs(int r, int c, int[][] maps, boolean[][] visited) {
+        int[] dr = {0,0,1,-1};
+        int[] dc = {1,-1,0,0};
         
-        visited[0][0] = true;
-        Deque<Point> q = new ArrayDeque<>();
-        q.offer(new Point(0,0,1));
-        
+        Deque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{r,c,1});
+        visited[r][c] = true;
         while(!q.isEmpty()) {
-            Point cur = q.poll();
-            for(int i =0 ;i < 4; i++) {
-                int nr = cur.r + dr[i];
-                int nc = cur.c + dc[i];
+            
+            int[] cur =q.poll();
+            for(int i= 0; i < 4; i++) {
+                int nr = cur[0] + dr[i];
+                int nc = cur[1] + dc[i];
                 
-          
-                
-                if((0<=nr && nr < rowLen) && (0 <= nc && nc < colLen)) {
-                    if(maps[nr][nc] == 1 && nr == rowLen-1 && nc == colLen-1) {
-                        return cur.step+1;
-                    }
-                    
+                if( (0 <= nr && nr < maps.length) && (0<= nc && nc < maps[0].length) ) {
                     if(!visited[nr][nc] && maps[nr][nc] == 1) {
-                        q.offer(new Point(nr,nc,cur.step+1));
+                        // System.out.println(nr + " " + nc + " " + cur[2]);
+                        if(nr == maps.length-1 && nc == maps[0].length-1) {
+                            ans = cur[2]+1;
+                            return;
+                        }
+                        
                         visited[nr][nc] = true;
-                            
+                        
+                        q.offer(new int[]{nr,nc,cur[2]+1});
                     }
                 }
+                
+                
             }
+            
         }
         
-        return -1;
-    }
-    public static class Point {
-        int r, c, step;
-        Point(int r, int c, int step) {
-            this.r = r;
-            this.c = c;
-            this.step = step;
-        }
+        ans = -1;
+        return ;
     }
 }
