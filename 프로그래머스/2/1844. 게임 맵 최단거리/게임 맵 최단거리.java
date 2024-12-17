19c2bec14db635cc76f826a63713a5f531b2ba8e
Wooseok <wooseokbird@gmail.com>
2024-12-17 16:26:30 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.53 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,47 +1,38 @@
 import java.util.*;
 class Solution {
-    static int ans = 0;
+    int ans = 0;
     public int solution(int[][] maps) {
         
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
-        bfs(0,0,maps, visited);
         
-        return ans;
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        return bfs(0,0,visited,maps);
     }
-    public static void bfs(int r, int c, int[][] maps, boolean[][] visited) {
+    public static int bfs(int r, int c, boolean[][] visited, int[][] maps) {
         int[] dr = {0,0,1,-1};
         int[] dc = {1,-1,0,0};
-        
-        Deque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{r,c,1});
+        Deque<Integer[]> q = new ArrayDeque<>();
+        q.offer(new Integer[]{r,c,1});
         visited[r][c] = true;
         while(!q.isEmpty()) {
-            
-            int[] cur =q.poll();
-            for(int i= 0; i < 4; i++) {
+            Integer[] cur = q.poll();
+            for(int i= 0; i <4; i++) {
                 int nr = cur[0] + dr[i];
                 int nc = cur[1] + dc[i];
-                
-                if( (0 <= nr && nr < maps.length) && (0<= nc && nc < maps[0].length) ) {
+                if( (0 <= nr && nr < maps.length)  && (0 <= nc && nc < maps[0].length) ) {
                     if(!visited[nr][nc] && maps[nr][nc] == 1) {
-                        // System.out.println(nr + " " + nc + " " + cur[2]);
-                        if(nr == maps.length-1 && nc == maps[0].length-1) {
-                            ans = cur[2]+1;
-                            return;
+                        // System.out.println(nr + " " + nc + " " + cur[2] + " " +maps.length + " " );
+                        if(nr == maps.length-1 && nc == maps[0].length - 1) {
+                            return cur[2] + 1 ;
                         }
-                        
                         visited[nr][nc] = true;
+                        q.offer(new Integer[]{nr, nc, cur[2] + 1});
                         
-                        q.offer(new int[]{nr,nc,cur[2]+1});
                     }
                 }
-                
-                
             }
-            
         }
         
-        ans = -1;
-        return ;
+        return -1;
+        
     }
 }
