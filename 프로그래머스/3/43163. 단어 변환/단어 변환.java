62201a3d80adc141787c50de9e2637893a2ae809
Wooseok <wooseokbird@gmail.com>
2024-12-17 14:53:21 +0900

    [level 3] Title: 단어 변환, Time: 0.68 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,50 +1,46 @@
 import java.util.*;
-
 class Solution {
-    public int solution(String begin, String target, String[] words) {
-        int ans = 0;
+    
+    public int solution(String begin, String target, String[] words) {       
+        boolean[] visited = new boolean[words.length];    
+        return bfs(begin,visited, target, words );
+    }
+    public static int bfs(String begin, boolean[] visited, String target, String[] words) {
         
-        int n = words.length;
-        boolean[] visited = new boolean[n];
         Deque<Info> q = new ArrayDeque<>();
         q.offer(new Info(0, begin));
-        
         while(!q.isEmpty()) {
             Info cur = q.poll();
-            if(cur.word.equals(target)) return cur.step;
-            
-            for(int i = 0; i < words.length; i++) {
-                String word = words[i];
-                // 현재 단어 아니면 diff계산.
-                if(!word.equals(cur.word)) {
+            if(cur.word.equals(target)) 
+                return cur.step;    
+            for(int i= 0; i< words.length; i++) {
+                if(!cur.word.equals(words[i])) {
                     int diff = 0;
-                    for(int j =0 ; j < word.length(); j++) {
-                        int front = word.charAt(j);
+                    for(int j = 0; j < words[i].length(); j++) {
+                        int front = words[i].charAt(j);
                         int back = cur.word.charAt(j);
                         if(front != back)
                             diff++;
                     }
                     
-                    if(diff == 1 && !visited[i]) {
+                    if(diff==1 && !visited[i]) { // 차이 1, 방문안한 word
                         visited[i] = true;
-                        q.offer(new Info(cur.step+1, word));
+                        q.offer(new Info(cur.step+1, words[i]));
                     }
                     
                 }
-                
             }
+            
+                
+            
+            
+            
         }
-            
-        
-            
-  
-        
-        
         return 0;
+        
     }
-    
     public static class Info {
-        int step;
+        int step; 
         String word;
         Info(int step, String word) {
             this.step = step;
