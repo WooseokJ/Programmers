20745923aa938796c55c19b69abca94688b48177
Wooseok <wooseokbird@gmail.com>
2024-12-18 17:20:08 +0900

    [level 1] Title: 인기있는 아이스크림, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/133024. 인기있는 아이스크림/인기있는 아이스크림.sql
@@ -0,0 +1,14 @@
+-- 코드를 입력하세요
+SELECT FLAVOR
+from FIRST_HALF
+order by TOTAL_ORDER desc, SHIPMENT_ID
+
+
+
+# FIRST_HALF 상반기 주문 정보
+# SHIPMENT_ID, FLAVOR, TOTAL_ORDER
+# 출하 번호, 아이스크림 맛, 상반기 아이스크림 총주문량
+
+
+# 상반기에 판매된 아이스크림의 맛을 총주문량을 기준으로 내림차순 정렬하고 총주문량이 같다면 출하 번호를 기준으로 오름차순 정렬하여 조회하는 SQL 문을 작성해주세요.
+
