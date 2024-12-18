d501dc1dbbd1bd207235151faf93998671dac914
Wooseok <wooseokbird@gmail.com>
2024-12-18 18:02:29 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -0,0 +1,14 @@
+-- 코드를 입력하세요
+SELECT animal_id, name
+from ANIMAL_INS
+where INTAKE_CONDITION = 'Sick'
+order by animal_id
+
+
+
+# ANIMAL_INS
+# ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE
+# 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부
+
+# 동물 보호소에 들어온 동물 중 아픈 동물의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 이때 결과는 아이디 순으로 조회해주세요.
+
