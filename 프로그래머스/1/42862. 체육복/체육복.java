f5d705dffbc071d2919dfdbacc8f4a7d582c50c8
Wooseok <wooseokbird@gmail.com>
2024-12-20 16:10:24 +0900

    [level 1] Title: 체육복, Time: 0.14 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,52 +1,39 @@
-import java.util.*;
 
+import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
+        int ans = 0;
+        Set<Integer> lostSet = new HashSet<>();
+        Set<Integer> reserveSet = new HashSet<>();
         
-        int ans = n - lost.length;
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-        
-
-        // 빌려줄수있는놈이 잃어버린경우(reserve -1로 바꿔서 못빌려줘)
-        for(int i =0; i < lost.length; i++) {
-            for(int j =0; j < reserve.length; j++) {
-                if(lost[i] == reserve[j]) {
-                    reserve[j] = -1;
-                    lost[i] = -1;
-                    ans++;
-                    break;
-                }
-            }
+        for(int r: reserve)
+            reserveSet.add(r);
+        for(int l: lost) {
+            // 빌려줄수있는놈이 잃어버리면 -> 자기가 써야해서 못빌려줘
+            if(reserveSet.contains(l))
+                reserveSet.remove(l);
+            else
+                lostSet.add(l);
         }
-
-        // System.out.println(ans + " " + Arrays.toString(lost) + " " + Arrays.toString(reserve) );
-  
-        // 3 -> 2,4번  
-        // 1 -> 0, 2번.. 
-        
-        // 잃어버린애 빌려주기(reserve +1,-1 해서 lost일치하는지 보기.)
-        for(int i= 0; i < lost.length; i++) {
-            for(int j =0; j < reserve.length; j++) {
-                if(lost[i] == reserve[j] + 1 || 
-                    lost[i] == reserve[j] -1) {
-                    
-                    // lost[i] = -1;
-                    reserve[j] = -1;
-                    ans++;
-                    
-                    break;
-                   
-                }
+        // 빌려줄수있는놈이 잃어버린놈 빌려줘
+        for(int r: reserveSet) { // 3
+            // 2
+            if(lostSet.contains(r-1)) {
+                 lostSet.remove(r-1); // 빌려주므로 lostSet은 삭제   
             }
+            // 4
+            else if(lostSet.contains(r+1)) {
+                lostSet.remove(r+1); // 마찬가지 
+            }
+            
         }
-   
-        // System.out.println(ans + " " + Arrays.toString(lost) + " " + Arrays.toString(reserve) );
         
         
         
-        
-        
+        ans = n - lostSet.size();
         return ans;
     }
 }
+
+// 4 -> 3, 5
+// 
