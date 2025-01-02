969ad991affd082bc1e501806fcc73e74e1ac2ce
Wooseok <wooseokbird@gmail.com>
2025-01-02 15:14:06 +0900

    [level 3] Title: 단속카메라, Time: 9.22 ms, Memory: 58.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,15 +1,13 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[][] routes) {
-				int ans =0;
+        
+        int ans =0;
         int cam = Integer.MIN_VALUE;
         
         Queue<Info> pq = new PriorityQueue<>();
-        for(int[] route: routes){
-            int start = route[0];
-            int end = route[1];
-            pq.offer(new Info(start, end));
+        for(int[] r: routes) {
+            pq.offer(new Info(r[0], r[1]));
         }
         
         while(!pq.isEmpty()) {
@@ -19,23 +17,21 @@ class Solution {
                 ans++;
             }
         }
-        
         return ans;
+            
     }
-    
-    
-    
-    public static class Info implements Comparable<Info> {
+    public static class Info implements Comparable<Info>{
         int start, end;
-        Info(int start, int end) {
+        Info(int start, int end){
             this.start = start;
             this.end = end;
+            
         }
-        
         @Override
         public int compareTo(Info o) {
             return this.end - o.end;
         }
             
+        
     }
 }
