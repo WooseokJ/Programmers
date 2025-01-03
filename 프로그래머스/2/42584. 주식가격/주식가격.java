6ac65a7df7825b66735a32206d10f49c9fe33775
Wooseok <wooseokbird@gmail.com>
2025-01-03 16:09:05 +0900

    [level 2] Title: 주식가격, Time: 29.82 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,23 +2,25 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] ans = new int[prices.length];
-        Deque<Integer> q = new ArrayDeque<>();
-        for(int a: prices)
-            q.offer(a);
         
-        int idx = 0;
+        Deque<Integer> q = new ArrayDeque<>();
+        
+        for(int t: prices)
+            q.offer(t);
+        
+        
         while(!q.isEmpty()) {
             int curPrice = q.poll();
-            // 현재idx+1시작~ 끝까지 
-            for(int i= idx+1; i < prices.length; i++) {
-                ans[idx]++; // 
-                // 현재가격보다 떨어진경우 -> 탈출.
-                if(curPrice > prices[i]) {
+            int start = (prices.length-1) - q.size();
+            for(int i = start+1; i < prices.length; i++) {
+                ans[start]++;
+                // 가격 떨궈져
+                if(curPrice > prices[i])
                     break;
-                }
             }
-            idx++;
         }
+        
+        
         
         return ans;
     }
