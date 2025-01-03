f0077a3389c9f018489b17d440740565db544903
Wooseok <wooseokbird@gmail.com>
2025-01-03 15:56:41 +0900

    [level 2] Title: 괄호 회전하기, Time: 48.32 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,59 +1,44 @@
 import java.util.*;
 class Solution {
     public int solution(String s) {
-        int[] ans = {0};
-        
-        int n = s.length();
-        
-        for(int x =0; x < n; x++) {
+        int ans = 0;
+        for(int i= 0 ; i < s.length(); i++) {
             StringBuilder sb = new StringBuilder();
-            for(int i = x; i < n ;i++) {
-                sb.append(s.charAt(i));
-            }
+            Deque<Character> stack = new ArrayDeque<>();
+            sb.append(s.substring(i,s.length())); // i~ 끝
+            sb.append(s.substring(0,i)); // 0~i-1까지 
             
-            for(int i = 0; i < x; i++) {   
-                char word = s.charAt(i);    
-                sb.append(word);
-            }
-            // System.out.println(sb);
-            
-            if(isValid(sb)) {
-                ans[0]++;
-            }
-        }
-        
-        return ans[0];
-    }
-    public static boolean isValid(StringBuilder sb) {
-        Deque<Character> stack = new ArrayDeque();
-        
-        for(int i = 0; i < sb.length(); i++) {
-            char word = sb.charAt(i);
-            
-            if(word == '[') {
-                stack.offerLast(']');
-            } else if(word == '(') {
-                stack.offerLast(')');
-            } else if(word == '{') {
-                stack.offerLast('}');
-            } else {     // ], ), }
-                
-                if(!stack.isEmpty() && (stack.peekLast() == word)) {
-                    stack.pollLast();
-                } else {
-                    return false;
+            boolean flag = true;
+            // 새로운 거부터 순회
+            for(int j = 0; j < sb.length(); j++) {
+                char c = sb.charAt(j);
+                if(c =='(') 
+                    stack.push(')');
+                 else if(c == '{')
+                     stack.push('}');
+                else if(c=='[') 
+                    stack.push(']');
+                else{ // 닫힌괄호들
+                    // 비엇으면 혹은 비어있진않은데 원소가 달라.
+                    if(stack.isEmpty() || (!stack.isEmpty() && stack.peek()!=c)) { 
+                        flag = false;
+                        break;
+                    }                    
+                    else { // 스택에 원소는 무조건있음
+                        if(stack.peek() == c) {
+                             stack.pop();
+                        }
+                    }
+                    
                 }
+                
             }
+            if(flag && stack.isEmpty())
+                ans++;
+            
+            
         }
         
-        
-        if(stack.isEmpty()) {
-            return true;
-        } else {
-            return false;
-        }
-      
-        
-        
+        return ans;
     }
 }
