ee0cc98bfcb0827d0532ff8fe45df78ead2e7f1f
Wooseok <wooseokbird@gmail.com>
2025-01-04 13:21:14 +0900

    [level 2] Title: 기능개발, Time: 0.23 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,30 +3,34 @@ class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         List<Integer> ans = new ArrayList<>();
         Deque<Integer> q = new ArrayDeque<>();
-        
-        for(int i=0; i < progresses.length; i++) {
-            int cnt = 1;
-            while(100 > progresses[i] + (cnt) * speeds[i]) 
-                cnt++;
-            q.add(cnt);
-        }
-        // 7 3 9
-        while(!q.isEmpty()) {
-            Integer stand = q.poll();
+        for(int i =0; i < progresses.length; i++) {
             int cnt =1;
-            
+            while(100 > progresses[i] + (cnt * speeds[i]) ) {
+                cnt++;
+            }
+            q.add(cnt);   
+        }
+        
+        while(!q.isEmpty()) {
+            int stand = q.poll();
+            int cnt =1;
             while(!q.isEmpty() && q.peek() <= stand) {
                 cnt++;
                 q.poll();
             }
             ans.add(cnt);
         }
-          
-        int[] t = new int[ans.size()];
-        for(int i=0; i < ans.size(); i++)
+
+        int[] t= new int[ans.size()];
+        for(int i =0; i < ans.size();i++)
             t[i] = ans.get(i);
             
+        
         return t;
+        
+        
         
     }
 }
+
+
