f9fe3ce22edbe8e074b1f9454825f0935b5db306
Wooseok <wooseokbird@gmail.com>
2025-01-13 16:22:29 +0900

    [level 3] Title: 섬 연결하기, Time: 3.13 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,48 +2,49 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] costs) {
         int ans = 0;
-        
-        Map<Integer, List<A>> map = new HashMap<>();
-        Queue<Integer[]> pq = new PriorityQueue<>((o1,o2) -> {
-            return o1[1]-o2[1];
-        });
-        for(int[] cost: costs){
+        Map<Integer, List<Info>> map = new HashMap<>();
+        for(int[] cost: costs) {
             map.putIfAbsent(cost[0], new ArrayList<>());
             map.putIfAbsent(cost[1], new ArrayList<>());
-            map.get(cost[0]).add(new A(cost[1], cost[2]));
-            map.get(cost[1]).add(new A(cost[0], cost[2]));
+            map.get(cost[0]).add(new Info(cost[1], cost[2]));
+            map.get(cost[1]).add(new Info(cost[0], cost[2]));
         }
-        boolean[]visited = new boolean[n];
-        
-        pq.add(new Integer[]{0,0});
+        Queue<Info> pq = new PriorityQueue<>(); // 가중치 작은순서로 오름차순.
+        pq.offer(new Info(0,0));
+        boolean[] visited = new boolean[n];
         while(!pq.isEmpty()) {
-            Integer[] cur = pq.poll();
-            int curNode = cur[0];
-            int weight = cur[1];
-            if(visited[curNode]) continue;
-            visited[curNode] = true;
-            ans+=weight;
-            if(map.containsKey(curNode)) {
-                List<A> a = map.get(curNode);
-                for(A b: a) {
-                   if(!visited[b.nextNode]) {
-                    pq.add(new Integer[]{b.nextNode, b.weight});
-                    }
+            Info cur = pq.poll();
+            
+            if(visited[cur.node]) continue;
+            visited[cur.node] = true;
+            ans+= cur.weight;
+            
+            if(map.containsKey(cur.node)) {
+                for(Info i: map.get(cur.node)) {
+                    int next = i.node;
+                    int weight =i.weight;
+                    pq.offer(new Info(next, weight));
                 }
-             
+                    
             }
-            
-            
-            
         }
+        
+            
+        
         
         return ans;
     }
-    public static class A {
-        int nextNode, weight;
-        A(int n, int w) {
-            this.nextNode = n;
-            this.weight = w;
+
+
+    public static class Info implements Comparable<Info>{
+        int node, weight;
+        Info(int n, int w) {
+            node =n;
+            weight =w;
+        }
+        @Override
+        public int compareTo(Info o) {
+            return this.weight - o.weight;
         }
     }
 }
