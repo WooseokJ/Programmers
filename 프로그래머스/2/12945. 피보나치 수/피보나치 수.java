ebac2ac7a88ec3a67457ea6e67d8c5e5d9a634d0
Wooseok <wooseokbird@gmail.com>
2025-01-13 19:28:28 +0900

    [level 2] Title: 피보나치 수, Time: 2.29 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,17 +1,12 @@
-
 class Solution {
     public int solution(int n) {
-        int ans = 0;
         int[] dp = new int[n+1];
-        
         dp[0] = 0;
         dp[1] = 1;
-        int mod = 1234567;
-        
-        for(int i = 2; i <= n; i++) {
-            dp[i] = (dp[i-1] + dp[i-2]) % mod;
+        for(int i=2; i <= n; i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
         }
+        
         return dp[n];
     }
-
 }
