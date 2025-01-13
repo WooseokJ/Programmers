86978ef9a9f247e99de87718d1d1db1b8aba27af
Wooseok <wooseokbird@gmail.com>
2025-01-13 19:48:48 +0900

    [level 3] Title: 정수 삼각형, Time: 10.74 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -2,25 +2,26 @@ import java.util.*;
 class Solution {
     public int solution(int[][] triangle) {
         int ans = 0;
-        int rowLen = triangle.length;
-        int[][] dp = new int[rowLen][rowLen];
+        int[][] dp = new int[triangle.length][triangle.length];
         
         dp[0][0] = triangle[0][0];
+        
+        for(int i=1; i< triangle.length; i++)
+            dp[i][0] = dp[i-1][0]+ triangle[i][0];
             
-        for(int i= 1;i < rowLen; i++) {
-            dp[i][0] = dp[i-1][0] + triangle[i][0];
-        }
-   
-        
-        for(int i =1; i < rowLen; i++) {
-            for(int j = 1; j < triangle[i].length; j++) {
-                dp[i][j] = Math.max(dp[i-1][j] + triangle[i][j], 
-                                    dp[i-1][j-1] + triangle[i][j]);
+        for(int i=1; i < triangle.length; i++)
+            for(int j= 1; j < triangle[i].length; j++) {
+                
+                dp[i][j] = Math.max(dp[i-1][j] + triangle[i][j],
+                                   dp[i-1][j-1] + triangle[i][j]);
             }
-        }
+                // 위+현재 , 현재 + 왼쪽위  
+                
+            
+
         
-        for(int i = 0; i < rowLen; i++) {
-            ans = Math.max(ans, dp[rowLen-1][i]);
+        for(int i = 0; i < triangle[triangle.length-1].length; i++) {
+            ans = Math.max(ans, dp[triangle.length-1][i]);
         }
         return ans;
     }
