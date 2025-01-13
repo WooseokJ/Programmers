7b6d9689648fb0734a6f1f9a1ede4f0c033559d5
Wooseok <wooseokbird@gmail.com>
2025-01-13 20:48:52 +0900

    [level 3] Title: 등굣길, Time: 1.21 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -16,17 +16,11 @@ class Solution {
         
         for(int i = 1; i < n+1; i++) {
             for(int j = 1; j < m+1; j++) {
-                
-                
-
                 if(dp[i][j] == -100) continue;
-                
                 if(i == 1 && j == 2)
                     dp[i][j] = 1;
                 if(i == 2 && j == 1)
                     dp[i][j] = 1; 
-                    
-                
                 // 왼쪽에 숫자 0 아니면 
                 if((dp[i][j-1] != 0) && dp[i][j-1] != -100) {
                     dp[i][j] += (dp[i][j-1]) % mod;
@@ -34,13 +28,12 @@ class Solution {
                 // 위쪽에 숫자 0 아니면
                 if((dp[i-1][j] != 0) && dp[i-1][j] != -100) {
                     dp[i][j] += (dp[i-1][j]) % mod;
-                }
-                    
+                }   
             }
         }
         
-//       for(int[] d : dp)
-//             System.out.println(Arrays.toString(d));
+      // for(int[] d : dp)
+            // System.out.println(Arrays.toString(d));
         
         return dp[n][m] % mod;
     }
