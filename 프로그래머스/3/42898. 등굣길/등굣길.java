3f53ea36ca8a015ade537d9b0d476414e9a1b5b6
Wooseok <wooseokbird@gmail.com>
2025-01-16 21:42:13 +0900

    [level 3] Title: 등굣길, Time: 1.21 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,41 +1,50 @@
 import java.util.*;
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int ans = 0;
-        int mod = 1000000007;
-        
+        int mod = 1_000_000_007;
         int[][] dp = new int[n+1][m+1];
-        dp[1][1] = 0;
-        
         for(int[] p: puddles) {
-            int x = p[1];
-            int y = p[0];
-            dp[x][y] = -100;
+            int m1 = p[0];
+            int n1 = p[1];
+            dp[n1][m1] = -1;
         }
-  
         
-        for(int i = 1; i < n+1; i++) {
-            for(int j = 1; j < m+1; j++) {
-                if(dp[i][j] == -100) continue;
-                if(i == 1 && j == 2)
-                    dp[i][j] = 1;
-                if(i == 2 && j == 1)
-                    dp[i][j] = 1; 
-                // 왼쪽에 숫자 0 아니면 
-                if((dp[i][j-1] != 0) && dp[i][j-1] != -100) {
-                    dp[i][j] += (dp[i][j-1]) % mod;
+        
+        for(int i= 1; i <= n; i++) {
+            for(int j =1; j <= m; j++) {
+                if(dp[i][j] == -1) {
+                    continue;
                 }
-                // 위쪽에 숫자 0 아니면
-                if((dp[i-1][j] != 0) && dp[i-1][j] != -100) {
-                    dp[i][j] += (dp[i-1][j]) % mod;
-                }   
+                if(i == 1 && j == 1) {
+                    dp[i][j] = 1 % mod;
+                    continue;
+                }
+                
+                
+                // 위랑 왼쪽 다 -1 우물일떄 
+                if(dp[i][j-1] == -1 && dp[i-1][j] == -1) {
+                    dp[i][j] = -1 ;
+                    continue;
+                }
+                if(dp[i][j-1] == -1) {
+                    dp[i][j] = dp[i-1][j] % mod;
+                }
+                
+                else if(dp[i-1][j] == -1) {
+                    dp[i][j] = dp[i][j-1] % mod;  
+                } 
+                
+                else {
+                    dp[i][j] = (dp[i][j-1] % mod) + (dp[i-1][j] % mod);    
+                    dp[i][j] %= mod;
+                }
             }
         }
         
-      // for(int[] d : dp)
-            // System.out.println(Arrays.toString(d));
+        // for(int[] p: dp)
+        //     System.out.println(Arrays.toString(p));
+        
         
         return dp[n][m] % mod;
     }
 }
-// return 최단경로 개수 % 1000_000_007
