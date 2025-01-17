2576714195cb06924f9d77565898c840461531f5
Wooseok <wooseokbird@gmail.com>
2025-01-17 16:08:55 +0900

    [level 0] Title: 평행, Time: 0.69 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -1,32 +1,27 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[][] dots) {
-        int ans = 0;
+        double l1 = 0;
         Arrays.sort(dots, (o1,o2) -> {
-            return o1[0] - o2[0];
-        });
-        Arrays.sort(dots, (o1,o2) -> {
-            return o1[0] - o2[0];
+           return o1[0]-o2[0]; 
         });
         
-        double l = 0;
-        for(int i=1;i < 4; i++) {
-            double x1 = (double) dots[i][0];
-            double y1 = (double) dots[i][1];
-            double x2 = (double) dots[i-1][0];
-            double y2 = (double) dots[i-1][1];
-            double newL = (y2-y1) / (x2-x1);
-            if(l == 0) {
-                l = newL;
+        for(int i =1; i < 4; i++) {
+            int x1 = dots[i][0];
+            int y1 = dots[i][1];
+            int x2 = dots[i-1][0];
+            int y2 = dots[i-1][1];
+            double l2 = (double) (y2-y1) / (x2-x1);
+            if(l1 == 0) {
+                l1 = l2;
                 i++;
-            } else if(l == newL) return 1;
-                
+            } else if(l1 == l2) {
+                return 1;
+            }
+            
         }
-
-        return ans;
+        
+        return 0;
     }
-
-    
-    
 }
-// 기울기: (y2-y1) / (x2-x1)
