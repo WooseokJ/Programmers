e79105a62281583c2aa057ea6e8b3f03b8e09511
Wooseok <wooseokbird@gmail.com>
2025-01-17 16:39:49 +0900

    [level 2] Title: 전화번호 목록, Time: 369.25 ms, Memory: 206 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,6 +1,21 @@
+import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        return answer;
+        Set<String> set = new HashSet<>();
+        for(String str: phone_book)
+            set.add(str);
+        
+        for(String num : phone_book) {
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0;i < num.length(); i++) {
+                char c = num.charAt(i);
+                sb.append(c);
+                String str = sb.toString();
+                if(set.contains(str) && !num.equals(str))
+                    return false;
+            }
+        }
+        return true;
+        
     }
 }
