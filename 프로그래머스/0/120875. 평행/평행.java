d7cc5d9a15ae0db713d5ce10ce88035903261a22
Wooseok <wooseokbird@gmail.com>
2025-01-17 12:00:48 +0900

    [level 0] Title: 평행, Time: 1.06 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -5,22 +5,24 @@ class Solution {
         Arrays.sort(dots, (o1,o2) -> {
             return o1[0] - o2[0];
         });
+        Arrays.sort(dots, (o1,o2) -> {
+            return o1[0] - o2[0];
+        });
         
-        
-        double l = -1;
-        for(int i =1; i < 4; i++) {
+        double l = 0;
+        for(int i=1;i < 4; i++) {
             double x1 = (double) dots[i][0];
             double y1 = (double) dots[i][1];
             double x2 = (double) dots[i-1][0];
             double y2 = (double) dots[i-1][1];
-            // System.out.println(x1 + " " + y1 + " " + x2 + " " + y2 + " "+ l + " " + (double)((y2-y1) / (x2-x1)));
-            if(l == -1.0) {
-                l = (y1-y2) / (x1-x2);
+            double newL = (y2-y1) / (x2-x1);
+            if(l == 0) {
+                l = newL;
                 i++;
-            } else if(l == ((y1 - y2) / (x1-x2))){
-                return 1;
-            } 
+            } else if(l == newL) return 1;
+                
         }
+
         return ans;
     }
 
