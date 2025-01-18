943da18a8556f0a9f7498252db9d6a1d4b6385f6
Wooseok <wooseokbird@gmail.com>
2025-01-18 11:46:09 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.10 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/README.md
+++ 프로그래머스/1/86051. 없는 숫자 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 없는 숫자 더하기 - 86051 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86051?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86051) 
 
 ### 성능 요약
 
-메모리: 78.8 MB, 시간: 0.06 ms
+메모리: 85.6 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 29일 17:16:16
+2025년 01월 18일 11:46:08
 
 ### 문제 설명
 
--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -2,18 +2,15 @@ import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
         int ans = 0;
-        
         Set<Integer> set = new HashSet<>();
-        for(int num: numbers){
-            if(!set.contains(num))
-                set.add(num);
-        }
+        for(int i= 0;i <= 9; i++)
+            set.add(i);
         
-        for(int i= 0; i< 10; i++) {
-            if(!set.contains(i))
-                ans+=i;
-        }
-        
+        for(int n: numbers)
+            if(set.contains(n))
+                set.remove(n);
+        for(Integer n: set)
+            ans+=n;
         return ans;
     }
 }
