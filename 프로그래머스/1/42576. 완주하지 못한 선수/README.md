1cb74916a2e9dc1b9d4ba80392a53ad006b7bd14
Wooseok <wooseokbird@gmail.com>
2025-01-18 11:51:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.64 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 97.3 MB, 시간: 74.03 ms
+메모리: 96.3 MB, 시간: 86.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 29일 17:32:40
+2025년 01월 18일 11:51:15
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,33 +1,34 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String ans = "";
+        StringBuilder sb = new StringBuilder();
+        Map<String, Integer> map = new HashMap<>();
         
-        Map<String , Integer > map = new HashMap<>();
-        for(String str: completion) {
-            if(!map.containsKey(str)) {
-                map.put(str, 1);
+        for(String p: participant) {
+            if(!map.containsKey(p)) {
+                map.put(p, 1);
             } else {
-                map.put(str, map.get(str) + 1);
+                Integer n = map.get(p);
+                map.put(p, n+1);
+            }
+        }
+      
+        
+        for(String c: completion)
+            if(map.containsKey(c)) {
+               Integer n = map.get(c); 
+                map.put(c, n-1);
             }
                 
+        
+        for(String key: map.keySet()) {
+            Integer n = map.get(key);
+            if(n!=0)
+                sb.append(key);
         }
-
+                
         
-       for(String str: participant) {
-            if(!map.containsKey(str)){
-                return str;
-            } else {
-                Integer n = map.get(str);
-                if(n == 0) {
-                    return str;
-                } else {
-                    map.put(str,n -1);
-                }
         
-            }
-       }
-
-        return ans;
+        return sb.toString();
     }
 }
