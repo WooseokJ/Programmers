30a9550a36b2b32a51df238d2e9280dc65a38739
Wooseok <wooseokbird@gmail.com>
2025-01-20 12:30:39 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 10.50 ms, Memory: 90.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numbers) {
+        List<Integer> temp =new ArrayList<>();
+        List<List<Integer>> arr = new ArrayList<>();
+        backtrack(new ArrayList<>(), numbers.length, 2, arr);
+        for(List<Integer> a: arr) {
+            int idx1 = a.get(0);
+            int idx2 = a.get(1);
+            int sum = numbers[idx1] + numbers[idx2];
+            if(!temp.contains(sum))
+                temp.add(sum);            
+        }
+        
+        Collections.sort(temp);
+        System.out.println(temp);
+            
+        
+        int[] ans = new int[temp.size()];
+        for(int i=0; i< temp.size(); i++)
+            ans[i] = temp.get(i);
+            
+        
+        return ans;
+    }
+    public static void backtrack(List<Integer> curr, int n, int k, List<List<Integer>> ans) {
+        if(curr.size()==k) {
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        for(int i= 0; i< n; i++) {
+            if(!curr.contains(i)) {
+                curr.add(i);
+                 backtrack(curr, n, k, ans);
+                curr.remove(curr.size()-1);
+            }
+    
+        }
+    }
+}
