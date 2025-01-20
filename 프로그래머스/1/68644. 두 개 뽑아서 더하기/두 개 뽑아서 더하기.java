697a8f2cb459fb0b3439aeae728ec4a786a98da3
Wooseok <wooseokbird@gmail.com>
2025-01-20 16:19:40 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 8.55 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,40 +1,40 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
-        List<Integer> temp =new ArrayList<>();
+        
         List<List<Integer>> arr = new ArrayList<>();
-        backtrack(new ArrayList<>(), numbers.length, 2, arr);
+        backtrack(new ArrayList<>(), numbers.length, 2 , arr);
+        // for(List<Integer> a: arr)
+        //     System.out.println(a);
+        
+        List<Integer> temp = new ArrayList<>();
         for(List<Integer> a: arr) {
             int idx1 = a.get(0);
             int idx2 = a.get(1);
             int sum = numbers[idx1] + numbers[idx2];
             if(!temp.contains(sum))
-                temp.add(sum);            
+                temp.add(sum);
         }
-        
         Collections.sort(temp);
-        System.out.println(temp);
-            
+
         
         int[] ans = new int[temp.size()];
-        for(int i=0; i< temp.size(); i++)
+        for(int i= 0; i < temp.size(); i++)
             ans[i] = temp.get(i);
-            
-        
         return ans;
     }
+    
     public static void backtrack(List<Integer> curr, int n, int k, List<List<Integer>> ans) {
         if(curr.size()==k) {
             ans.add(new ArrayList<>(curr));
             return;
         }
-        for(int i= 0; i< n; i++) {
+        for(int i= 0;i < n ;i++) {
             if(!curr.contains(i)) {
                 curr.add(i);
-                 backtrack(curr, n, k, ans);
+                backtrack(curr, n, k, ans);
                 curr.remove(curr.size()-1);
             }
-    
         }
     }
 }
