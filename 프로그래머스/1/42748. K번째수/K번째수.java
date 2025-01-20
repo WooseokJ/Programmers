0e2614d3fb5d09fd1c556b7fb0914a3010dca9d7
Wooseok <wooseokbird@gmail.com>
2025-01-20 16:09:06 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,23 +1,24 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
+    
         List<Integer> arr = new ArrayList<>();
+        
         for(int[] command: commands) {
             int start = command[0];
             int end = command[1];
-            int k = command[2];
+            int k= command[2];
             List<Integer> temp = new ArrayList<>();
-            for(int i = start; i <= end; i++) {
+            for(int i=start; i <= end; i++) {
                 temp.add(array[i-1]);
             }
             Collections.sort(temp);
             arr.add(temp.get(k-1));
-            
         }
         
         
         int[] ans = new int[arr.size()];
-        for(int i= 0;i < arr.size();i++)
+        for(int i=0; i < arr.size(); i++)
             ans[i] = arr.get(i);
         return ans;
     }
