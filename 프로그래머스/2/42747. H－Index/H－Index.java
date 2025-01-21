0663cac513c88b2c5646ac76c60402278febb0b4
Wooseok <wooseokbird@gmail.com>
2025-01-21 16:10:01 +0900

    [level 2] Title: H-Index, Time: 1.67 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,24 +3,26 @@ class Solution {
     public int solution(int[] citations) {
         int ans = 0;
         List<Integer> arr = new ArrayList<>();
-        for(int i: citations)
-            arr.add(i);
+        for(int c: citations)
+            arr.add(c);
         
         Collections.sort(arr);
         
-        for(int i= 0; i < arr.size(); i++) {
-            int h = arr.size()-i;
-            int one = arr.get(i);
-            if(one >= h)
+        for(int i=0; i< arr.size(); i++) {
+            int h = arr.size() - i;
+            if(arr.get(i) >= h)
                 ans++;
             
-            // System.out.println(one + " " + h);
         }
         
         return ans;
     }
 }
 
+
 // n = citations.length
-// 5 4 3 2 1 
-// 0 1 3 5 6 
+// h편논문이 h편이상. 
+// citations[i] : 논문별 인용횟수 , i번이 citations[i]만큼 인용횟수 
+
+// 1번이 0 번만큼 인용됨. 
+// citations[i]>=i
