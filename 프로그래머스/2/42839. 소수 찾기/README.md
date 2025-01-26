fe018ae1a89d4cb1395886babba7faea458fb2cb
Wooseok <wooseokbird@gmail.com>
2025-01-26 14:54:46 +0900

    [level 2] Title: 소수 찾기, Time: 1522.87 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 83.8 MB, 시간: 104.25 ms
+메모리: 79.6 MB, 시간: 1522.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 26일 10:48:20
+2025년 01월 26일 14:54:43
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,53 +2,47 @@ import java.util.*;
 class Solution {
     public int solution(String numbers) {
         int ans = 0;
-        
-        int n= numbers.length();
-        Set<Integer> set = new HashSet<>();
         List<List<Integer>> arr = new ArrayList<>();
-        
-        for(int i=1; i <= n; i++ ) {
-            backtrack(new ArrayList<>(), n, i, arr);    
+        for(int i = 1; i<= numbers.length(); i++) {
+            backtrack(new ArrayList<>(), numbers.length(), i, arr);
         }
+        Set<Integer> set = new HashSet<>();
         for(List<Integer> a: arr) {
+            // System.out.println(a);
             StringBuilder sb = new StringBuilder();
-            for(Integer idx: a) {
-                char c = numbers.charAt(idx);
+            for(Integer b: a) {
+                char c = numbers.charAt(b);
                 sb.append(c);
             }
-            int num = Integer.parseInt(sb.toString());
-            if(set.contains(num)) continue;
-
-            if(isVaild(num)) {
-                ans++;
-                set.add(num);
+            int n = Integer.parseInt(sb.toString());
+            if(!set.contains(n)) {
+                set.add(n);
             }
-            // System.out.println(num +" " +  ans);    
-            // System.out.println(a);
         }
-            
+        for(Integer n: set) {
+            if(n == 0 || n == 1) continue;
+            int cnt =0;
+            for(int i=1; i<= n; i++) {
+                if(n % i == 0) {
+                    cnt++;
+                }
+            }
+            if(cnt == 2) {
+                ans++;
+            }
+        }
+        
+
         
         
         return ans;
     }
-    public static boolean isVaild(int n) {
-        if(n == 0 || n == 1) return false;
-        int cnt = 0;
-        for(int i =2; i < n; i++) {
-            if(n % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    
     public static void backtrack(List<Integer> curr, int n, int k, List<List<Integer>> ans) {
         if(curr.size() == k) {
             ans.add(new ArrayList<>(curr));
             return;
         }
-        for(int i =0; i < n; i++) {
+        for(int i=0;i < n; i++) {
             if(!curr.contains(i)) {
                 curr.add(i);
                 backtrack(curr, n, k, ans);
