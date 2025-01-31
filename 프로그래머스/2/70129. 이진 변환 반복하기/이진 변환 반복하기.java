b7515e4771f2002cd0ef48f7b83af2be023f182a
Wooseok <wooseokbird@gmail.com>
2025-01-31 20:50:28 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 7.17 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,47 +1,29 @@
-import java.util.*;
+
 class Solution {
     public int[] solution(String s) {
+        int[] ans = {0,0};
         
-        int deleteZero = 0;
-        int cnt = 0;
-        int t = 0;
         while(!s.equals("1")) {
-        // while(t < 3){
-            t++;
-            List<Character> arr = new ArrayList<>();
-
-            int zero = 0;
-            for(int i=0; i < s.length(); i++) {
-                char w = s.charAt(i);
-                if(w == '1') {
-                    arr.add(w);
+            int cnt = 0;
+            for(int i=0;i < s.length(); i++) {
+                if(s.charAt(i) == '0') {
+                    ans[1]++;
                 } else {
-                    zero++;
+                    cnt++;
                 }
             }
-            deleteZero+= zero;
-            int c = arr.size(); // 4
-            StringBuilder sb = new StringBuilder();
-            int a = 0;
-            while(c != 0){
-                a++;
-                sb.insert(0, c % 2);
-                c = c / 2;
-                // System.out.println(s + " " + c + " " + sb.toString());
-            }
-            
-
-            s = sb.toString();
-            cnt++;
-                
+            s = transform(cnt);
+            ans[0]++;
         }
-        
-
-        
-        int[] ans = new int[2];
-        ans[0] = cnt;
-        ans[1] = deleteZero;
         return ans;
     }
+    public static String transform(int cnt) {
+        StringBuilder sb = new StringBuilder();
+        while(cnt != 0) {
+            sb.append(cnt % 2);
+            cnt/=2;
+        }
+        return sb.toString();
+        
+    }
 }
-// return [이진변환횟수,제거된 0개수]
