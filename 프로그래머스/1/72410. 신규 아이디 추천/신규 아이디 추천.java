cdcfd851139458942466636fa788aff834625169
Wooseok <wooseokbird@gmail.com>
2025-02-01 19:36:52 +0900

    [level 1] Title: 신규 아이디 추천, Time: 2.45 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,70 +1,64 @@
-import java.util.*;
-
 class Solution {
     public String solution(String new_id) {
-        StringBuilder ans = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+        // 1단계 대 -> 소
+        for(int i =0; i < new_id.length(); i++) {
+            char c = new_id.charAt(i);
+            if(c >= 'A' && c <= 'Z') {
+                sb.append((char) (c + 32));
+            } else if(c == '-' || c == '_' || c == '.') {
+                sb.append(c);
+            } else if(Character.isDigit(c)) {
+                sb.append(c);
+            } else if( c>='a'  && c <= 'z') {
+                sb.append(c);
+            }
+        }
         
-        // 1단계 대 -> 소 
-        ans.append(new_id.toLowerCase());
 
-        // 2단계 소문자, 숫자, - , _ , . 제외 모든문자 제거.
-        for(int i = 0; i < ans.length(); i++) {
-            char word = ans.charAt(i);
-            // 소문자 혹은 숫자 
-            if(Character.isLowerCase(word) || Character.isDigit(word)) continue;
-            if(word == '-' || word == '_' || word == '.' )  continue;           
-            ans.deleteCharAt(i);
-            i--;
+        
+        // 3단계 .. -> .
+        while(sb.indexOf("..") != -1) {
+            int n = sb.indexOf("..");
+            sb.replace(n, n+2, ".");
+        }
+//         // 4단계 첫 . 제거 
+        if(sb.charAt(0) == '.') {
+            sb.deleteCharAt(0);
+        }
+        if( sb.length() != 0 && sb.charAt(sb.length()-1) == '.') {
+            sb.deleteCharAt(sb.length()-1);
         }
         
-        // 3단계  .. 가 연속이면 . 로 
-        while(ans.indexOf("..") != -1) {
-            int idx = ans.indexOf("..");
-            ans.replace(idx, idx+2, ".");
+//         // 5단계 빈문자열이면 a대입
+        if(sb.length() == 0) {
+            sb.append('a');
         }
-        
-        // 4단계 .가 처음이나 끝이면 제거 .
-        if(ans.charAt(0) == '.' && ans.length() > 0) {
-          ans.deleteCharAt(0);  
-        } 
-        
-        // 이거로는 오류 
-        // if(ans.charAt(ans.length() -1) == '.' && ans.length() > 0) {
-            // ans.deleteCharAt(ans.length()-1);  
-        // }
-        
-        // 이거로는 풀려 
-        if(ans.length() > 0 && ans.charAt(ans.length() -1) == '.') {
-            ans.deleteCharAt(ans.length()-1);
-        }
+//         // 6단계 16이상이면 
 
-        // 5단계 빈문자열은 "a"로 대입
-        if(ans.length() == 0) ans.append("a");
-        // 6단계 16글자 이상이면 15문자 제외 나머지 뒤에 제거 , 만약 제거후 . 가 끝에 위치하면 . 제거.
-        while(ans.length() >= 16) {
-            ans.replace(15, ans.length(), "");
-                   
-            if(ans.charAt(ans.length()-1) == '.' )
-                ans.deleteCharAt(ans.length()-1);
-        }
+        if(sb.length() >= 16) {
+            StringBuilder temp = new StringBuilder();
+            temp.append(sb.substring(0,15)); 
             
-     
-        
-        
-        
-        // 7단계 : 2자 이하면 글자길이 3 될떄까지 반복
-        while(ans.length() > 0 && ans.length() <= 2) {
-            char addWord = ans.charAt(ans.length()-1);
-            ans.append(addWord);
-        }
-        
-        
-        
+            if( temp.length() != 0 && temp.charAt(temp.length()-1) == '.') {
+                 temp.deleteCharAt(temp.length()-1);
+            }
             
+            sb.delete(0,sb.length());
+            sb.append(temp);
+        }
+//         System.out.println(sb.toString());
+
+        // 7단계
+        while(sb.length() <= 2) {
+            // System.out.println(sb.toString().charAt(sb.length()));
+            sb.append(sb.toString().charAt(sb.length() - 1));
+        }
+    
         
-        return ans.toString();
+        System.out.println(sb);
+
         
-        
-        
+        return sb.toString();
     }
 }
