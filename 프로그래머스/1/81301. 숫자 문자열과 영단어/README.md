81b903eca4c47a2da7c33b0b5eae7572ba007d0b
Wooseok <wooseokbird@gmail.com>
2025-02-01 18:37:49 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.47 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301) 
 
 ### 성능 요약
 
-메모리: 74.7 MB, 시간: 0.27 ms
+메모리: 83 MB, 시간: 0.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 23일 13:49:10
+2025년 02월 01일 18:37:47
 
 ### 문제 설명
 
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,41 +1,55 @@
 import java.util.*;
 class Solution {
     public int solution(String s) {
+
+        Map<String, Integer> map = new HashMap<>();
+        map.put("zero",0);
+        map.put("one",1);
+        map.put("two",2);
+        map.put("three",3);
+        map.put("four",4);
+        map.put("five",5);
+        map.put("six",6);
+        map.put("seven",7);
+        map.put("eight",8);
+        map.put("nine",9);
         
-        Map<String, Character> map = new HashMap<>();
-        map.put("zero",'0');
-        map.put("one",'1');
-        map.put("two",'2');
-        map.put("three",'3');
-        map.put("four",'4');
-        map.put("five",'5');
-        map.put("six",'6');
-        map.put("seven",'7');
-        map.put("eight",'8');
-        map.put("nine",'9');
+        Set<Character> set = new HashSet<>();
+        set.add('0');
+        set.add('1');
+        set.add('2');
+        set.add('3');
+        set.add('4');
+        set.add('5');
+        set.add('6');
+        set.add('7');
+        set.add('8');
+        set.add('9');
         
-        StringBuilder sb = new StringBuilder();
+        StringBuilder ans = new StringBuilder();
         StringBuilder temp = new StringBuilder();
-        for(int i= 0; i < s.length(); i++) {
-            char c= s.charAt(i);
-            if(!Character.isDigit(c)) {
+        
+        for(int i=0; i < s.length(); i++) {
+            char c=  s.charAt(i);
+            if(!set.contains(c))
                 temp.append(c);
-            } else {
-                sb.append(c);
-            }
+            else
+                ans.append(c);
+            
+            
             
             if(map.containsKey(temp.toString())) {
-                Character tt = map.get(temp.toString());
-                sb.append(tt);
+                int n = map.get(temp.toString());
+                ans.append(String.valueOf(n));
                 temp = new StringBuilder();
             }
             
         }
+        System.out.println(ans);
         
-
         
-        int ans = Integer.parseInt(sb.toString());
         
-        return ans;
+        
+        return Integer.parseInt(ans.toString());
     }
 }
