0ca823378405454ad82ee181e86cf2aa9bf37d5c
Wooseok <wooseokbird@gmail.com>
2025-02-17 13:43:39 +0900

    [난이도 2] Title: 구름이의 여행, Time: 0.13 ms, Memory: 33.71 MB -BaekjoonHub

--- goormlevel/159546/1. 구름이의 여행/구름이의 여행.java
+++ goormlevel/159546/1. 구름이의 여행/구름이의 여행.java
@@ -3,45 +3,45 @@ import java.util.*;
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer token =new StringTokenizer(br.readLine(), " ");
+		StringTokenizer token = new StringTokenizer(br.readLine(), " ");
 		int n = Integer.parseInt(token.nextToken());
 		int m = Integer.parseInt(token.nextToken());
 		int k = Integer.parseInt(token.nextToken());
-		
 		Map<Integer, List<Integer>> map = new HashMap();
 		for(int i=1;i <= n;i++)
 			map.put(i, new ArrayList<>());
-		
-		for(int i=0;i < m; i++) {
-			token =new StringTokenizer(br.readLine(), " ");
+		// 양방향 연결 
+		for(int i=0;i < m;i++) {
+			token = new StringTokenizer(br.readLine(), " ");
 			int u = Integer.parseInt(token.nextToken());
 			int v = Integer.parseInt(token.nextToken());
 			map.get(u).add(v);
 			map.get(v).add(u);
 		}
+		
 		Deque<Integer> q = new ArrayDeque();
 		q.offer(1);
-		int[] dp = new int[n+1];
-		Arrays.fill(dp, -1);
-		dp[1] = 0;
+		int[] visited = new int[n+1];
+		Arrays.fill(visited, -100);
+		visited[1] = 0;
 		while(!q.isEmpty()) {
 			int cur = q.poll();
-			for(int next: map.get(cur)) {
-				if(dp[next] == -1) {
-					dp[next] = dp[cur] + 1;
+			List<Integer> arr = map.get(cur);
+			for(int next: arr) {
+				// 방문한적 없어
+				if(visited[next] == -100) {  
+					visited[next] = visited[cur] + 1;
 					q.offer(next);
 				}
 			}
 		}
-		// System.out.println(Arrays.toString(dp));
-		if(dp[n] <= k && dp[n] >=0) {
+		if(1 <= visited[n] && visited[n] <= k) {
 			System.out.println("YES");
-		} else {
+		}else {
 			System.out.println("NO");
 		}
 		
-		
-		
+
 		
 	}
 }
