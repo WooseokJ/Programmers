964e6ccecc92b88d886ccd8542a4f218db833e71
Wooseok <wooseokbird@gmail.com>
2025-03-12 11:41:23 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -1,6 +1,9 @@
 -- 코드를 입력하세요
-SELECT count(distinct name)
-from ANIMAL_INS
+SELECT  count( distinct name) as count
+from animal_ins
+;
 
-# 동물 보호소에 들어온 동물의 이름은 몇 개인지 조회하는 SQL 문을 작성해주세요. 이때 이름이 NULL인 경우는 집계하지 않으며 중복되는 이름은 하나로 칩니다.
 
+# ANIMAL_INS
+# ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE
+# 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부
