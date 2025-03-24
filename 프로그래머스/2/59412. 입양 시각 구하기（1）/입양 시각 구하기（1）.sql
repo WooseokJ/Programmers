45a2d056d6e1fe1010259a1a71426022841801e0
Wooseok <wooseokbird@gmail.com>
2025-03-24 11:59:54 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,10 +1,16 @@
 -- 코드를 입력하세요
-SELECT hour(datetime) as hour, count(*)
+SELECT Hour(datetime) HOUR, count(*) COUNT
 from ANIMAL_OUTS
-where date_format(datetime, '%H%i') between '09:00' and '20:00'
-group by hour
-order by hour 
-;
+where date_format(DATETIME, '%H:%i') between '09:00' and '19:59'
+group by HOUR
+order by HOUR
+# select * 
+# from ANIMAL_OUTS
+# where date_format(DATETIME, '%H:%i') between '09:00' and '19:59'
+# order by datetime
 
+
+# ANIMAL_OUTS
 # ANIMAL_ID, ANIMAL_TYPE, DATETIME, NAME, SEX_UPON_OUTCOME
-# 동물의 아이디, 생물 종, 입양일, 이름, 성별 및 중성화 여부
+#  동물의 아이디, 생물 종, 입양일, 이름, 성별 및 중성화 여부
+
