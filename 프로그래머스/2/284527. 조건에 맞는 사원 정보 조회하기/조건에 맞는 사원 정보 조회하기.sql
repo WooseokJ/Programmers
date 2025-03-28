cede7d3d9258d9d582603b208e84b5bab5bd45a9
Wooseok <wooseokbird@gmail.com>
2025-03-28 17:31:56 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,8 +1,23 @@
 -- 코드를 작성해주세요
-select sum(score) as SCORE, hg.emp_no as EMP_NO, he.EMP_NAME as EMP_name, he.position as POSITION, he.email as EMAIL
-from HR_EMPLOYEES as he left join HR_GRADE as hg
-on he.emp_no = hg.emp_no
-group by hg.EMP_NO, hg.emp_no, he.emp_name, he.position, he.email
+# select hg.score, hg.EMP_NO,  he.emp_name,he.position, he.email
+select sum(hg.score) as score, HE.EMP_NO, HE.EMP_NAME, HE.POSITION, HE.EMAIL
+from hr_grade hg join HR_EMPLOYEES he
+on hg.emp_no = he.emp_no
+where hg.year = '2022'
+group by hg.EMP_NO
 order by score desc
 limit 1
-;
+
+# having max(hg.score) = hg.score
+
+# HR_DEPARTMENT
+# DEPT_ID, DEPT_NAME_KR, DEPT_NAME_EN, LOCATION
+# 부서 ID, 국문 부서명, 영문 부서명, 부서 위치
+
+# HR_EMPLOYEES
+# EMP_NO, EMP_NAME, DEPT_ID, POSITION, EMAIL, COMP_TEL, HIRE_DATE, SAL
+# 사번, 성명, 부서 ID, 직책, 이메일, 전화번호, 입사일, 연봉
+
+# HR_GRADE
+# EMP_NO, YEAR, HALF_YEAR, SCORE
+# 사번, 연도, 반기, 평가 점수
