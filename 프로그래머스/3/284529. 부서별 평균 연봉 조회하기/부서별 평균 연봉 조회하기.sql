3ce25f0fa80bb488daf4b7c00f1155fad5940151
Wooseok <wooseokbird@gmail.com>
2025-03-30 12:05:39 +0900

    [level 3] Title: 부서별 평균 연봉 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
+++ 프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
@@ -1,8 +1,16 @@
 -- 코드를 작성해주세요
+select hd.dept_id, hd.dept_name_en, round(avg(SAL),0) as AVG_SAL
+from hr_department hd join HR_EMPLOYEES he
+on hd.DEPT_ID = he.DEPT_ID
+group by hd.DEPT_ID
+order by avg_sal desc
 
-select hd.dept_id, hd.dept_name_en, round(avg(sal)) as avg_sal
-from HR_DEPARTMENT as hd join hr_employees as he
-on hd.dept_id = he.dept_id
-group by dept_id
-order by avg_sal desc 
-;
+
+# HR_DEPARTMENT
+# DEPT_ID, DEPT_NAME_KR, DEPT_NAME_EN, LOCATION
+# 부서 ID, 국문 부서명, 영문 부서명, 부서 위치
+
+# HR_EMPLOYEES
+# EMP_NO, EMP_NAME, DEPT_ID, POSITION, EMAIL, COMP_TEL, HIRE_DATE, SAL
+# 사번, 성명, 부서 ID, 직책, 이메일, 전화번호, 입사일, 연봉
+
