1018b9df8cc0edfd6a76019cf4d1370ad2cbee4e
Wooseok <wooseokbird@gmail.com>
2025-04-03 12:05:41 +0900

    [level 3] Title: 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -1,9 +1,11 @@
 -- 코드를 작성해주세요
-select count(fish_type) as fish_count, max(length) as max_length, fish_type
+
+select count(id) FISH_COUNT, max(LENGTH) max_length, fish_type
+# select AVG(length)
 from FISH_INFO
-group by fish_type
-having avg(coalesce(length, 10)) >= 33
-
-
-order by fish_type 
-;
+group by FISH_TYPE
+having avg(ifnull(length, 10)) >= 33
+order by fish_type
+# FISH_INFO
+# ID, FISH_TYPE, LENGTH, TIME
+# 잡은 물고기의 ID, 물고기의 종류(숫자), 잡은 물고기의 길이(cm), 물고기를 잡은 날짜
