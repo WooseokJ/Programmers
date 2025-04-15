29c684a52ee7d9bbec0f8aa91e75cd95bd8d6354
Wooseok <wooseokbird@gmail.com>
2025-04-15 14:34:57 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,17 +1,18 @@
 -- 코드를 입력하세요
-SELECT ao.ANIMAL_ID, ao.name
-from ANIMAL_INS as ai right join ANIMAL_OUTS as ao
-on ai.ANIMAL_ID = ao.ANIMAL_ID
-where ai.ANIMAL_ID is null
-order by animal_id
-;
+# SELECT animal_id
+select ANIMAL_OUTS.animal_id, ANIMAL_OUTS.name
+from ANIMAL_INS right join ANIMAL_OUTS
+on ANIMAL_INS.animal_id = ANIMAL_OUTS.animal_id
+where ANIMAL_INS.animal_id is null
 
+order by ANIMAL_INS.animal_id
 
-# 입양을 간 기록(outs)은 있는데, 보호소에 들어온(ins) 기록이 없는 동물의 ID와 이름을 ID 순으로 조회하는 SQL문을 작성해주세요.
+# 입영간 기록 
 
-
+# ANIMAL_INS: 보건소 들어온 
 # ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE
 # 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부
 
+# ANIMAL_OUTS: 보호소에서 입양보낸 (입영간)
 # ANIMAL_ID, ANIMAL_TYPE, DATETIME, NAME, SEX_UPON_OUTCOME
-# 동물의 아이디, 생물 종, 입양일, 이름, 성별 및 중성화 여부
+#  동물의 아이디, 생물 종, 입양일, 이름, 성별 및 중성화 여부
