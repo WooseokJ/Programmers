59815f4c2c9f5d8d5b69d4e8c15ea4534a5dd2de
Wooseok <wooseokbird@gmail.com>
2025-04-16 12:55:31 +0900

    [level 2] Title: 스킬트리, Time: 0.15 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -6,17 +6,29 @@ class Solution {
         for(String str: skill_trees) {
             StringBuilder sb = new StringBuilder();
             
-            for(int i= 0; i < str.length(); i++) {
-                String c = String.valueOf(str.charAt(i));
-                if(skill.contains(c))
+            for(int i=0;i < str.length(); i++) {
+                String c = String.valueOf(str.charAt(i));  
+                if(skill.contains(c)) {
                     sb.append(c);
+                    
+                }
+                    
             }
             
-            if(skill.indexOf(sb.toString()) == 0)
+            
+            
+            if(skill.indexOf(sb.toString()) == 0 ) {
                 ans++;
+            }
+            
+            
         }
-        
         
         return ans;
     }
 }
+
+// BACDE
+// CBADF -> CBD
+// AECB
+// BDA
